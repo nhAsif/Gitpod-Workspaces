36c97f3a9687b1cc0ef98f15dd7e38baff8a6949
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-05 13:35:15 +0600

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,5 +1,3 @@
 cd
 git clone --depth=1 https://github.com/nhAsif/kernel_xiaomi_rosy.git -b twelve kernel
 cd kernel 
-git remote add foo https://android.googlesource.com/kernel/common/
-git fetch foo
