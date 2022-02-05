871ec277dfe04dd72ff825bc57b9e1c80f1d0426
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-05 11:19:13 +0600

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,3 +1,4 @@
+git clone https://github.com/CyberJalagam/android_rom_building_scripts scripts && cd scripts && chmod +x build.sh && echo "1 13" | bash build.sh
 cd
 git clone https://github.com/nhAsif/kernel_xiaomi_rosy.git -b twelve kernel
 cd kernel 
