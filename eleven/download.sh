cb7e2271dbe83d3d3fa82e2a2e25208f7e38a2d4
levi3609 <99073465+levi3609@users.noreply.github.com>
2022-02-05 16:09:47 +0600

    Update download.sh

--- eleven/download.sh
+++ eleven/download.sh
@@ -4,7 +4,5 @@ echo "Downloading few Dependecies . . ."
 # Kernel Sources
 git clone --depth=1 https://github.com/nhAsif/kernel_xiaomi_rosy -b eleven $DEVICE_CODENAME
 # Toolchain
-git clone --depth=1 https://github.com/AnGgIt88/gcc-arm GCC32
-git clone --depth=1 https://github.com/AnGgIt88/gcc-arm64 GCC64
 git clone https://github.com/Pulkit077/gcc-arm64 -b gcc-master --depth=1 GCC64
 git clone https://github.com/Pulkit077/gcc-arm -b gcc-master --depth=1 GCC32
