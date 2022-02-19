6a408eab7fbc132532f4c89a1ccae86c878cc9d6
Asif <49162687+nhAsif@users.noreply.github.com>
2022-02-19 09:43:04 +0600

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -2,8 +2,8 @@ git clone https://github.com/CyberJalagam/android_rom_building_scripts scripts &
 cd ../
 mkdir rom
 cd rom
-repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
-git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b evo-10 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/Evolution-X/manifest -b ten -g default,-mips,-darwin,-notdefault
+git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b main .repo/local_manifests
 repo sync -j$(nproc --all) -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
 source build/envsetup.sh
 lunch aosp_rosy-userdebug
