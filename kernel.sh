3bd309dd21025a500daade8b454ab7a1d5ad66e4
Asif <49162687+nhAsif@users.noreply.github.com>
2022-08-24 19:58:22 +0600

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo apt install -y python3 pip
-git clone https://github.com/CyberJalagam/android_rom_building_scripts scripts && cd scripts && chmod +x build.sh && echo "1 13" | bash build.sh
 
